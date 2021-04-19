f58d55a818f42348c511d02201ae9d00259e4c9e
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-04-19 11:38:27 +0200

    Ajout formulaire prototype new group

--- SocialMediaProject/app/src/main/java/com/example/socialmediaproject/ui/newGroup/NewGroupFragment.java
+++ SocialMediaProject/app/src/main/java/com/example/socialmediaproject/ui/newGroup/NewGroupFragment.java
@@ -1,5 +1,6 @@
 package com.example.socialmediaproject.ui.newGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -7,8 +8,12 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -27,6 +32,11 @@ public class NewGroupFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.new_group_fragment, container, false);
 
+        // title fragment in the header
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Nouveau Groupe");
+        // affichage de la flèche retour en arrière dans le menu
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
         return view;
     }
 
@@ -36,5 +46,31 @@ public class NewGroupFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(NewGroupViewModel.class);
         // TODO: Use the ViewModel
     }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        // inflate menu
+        inflater.inflate(R.menu.close_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.close_menu_exit: // action sur la flèche de retour en arrière
+                getActivity().onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
