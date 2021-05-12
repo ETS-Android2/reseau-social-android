b3ec423fc474031e1c51ee509a25326e1ed00d65
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-05-12 12:39:21 +0200

    Changement du model de stockage des données (on a supprimé les subcollection pour mettre seulement des collections)

--- SocialMediaProject/app/src/main/java/com/example/socialmediaproject/api/PostHelper.java
+++ SocialMediaProject/app/src/main/java/com/example/socialmediaproject/api/PostHelper.java
@@ -2,6 +2,7 @@ package com.example.socialmediaproject.api;
 
 import com.example.socialmediaproject.models.Post;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -14,22 +15,23 @@ public class PostHelper {
 
     private static final String COLLECTION_NAME = "posts";
 
+    // --- COLLECTION REFERENCE ---
+    public static CollectionReference getPostCollection(){
+        return FirebaseFirestore.getInstance().collection(COLLECTION_NAME);
+    }
+
     // --- CREATE ---
     public static Task<DocumentReference> createPostForGroup(Post postToCreate){
         // 1 - Create the Message object
 
         // 2 - Store Message to Firestore
-        return GroupHelper.getGroupCollection()
-                .document(postToCreate.getGroup())
-                .collection(COLLECTION_NAME)
-                .add(postToCreate);
+        return PostHelper.getPostCollection().add(postToCreate);
     }
 
     // --- GET ---
     public static Query getAllPostForGroup(String name){
-        return GroupHelper.getGroupCollection()
-                .document(name)
-                .collection(COLLECTION_NAME)
+        return PostHelper.getPostCollection()
+                .whereEqualTo("group", name)
                 .orderBy("dateCreated")
                 .limit(50);
     }
@@ -40,8 +42,7 @@ public class PostHelper {
     }
 
     // --- DELETE ---
-    public static Task<Void> deletePost(String groupId, String postId) {
-        return GroupHelper.getGroupCollection().document(groupId)
-                .collection(COLLECTION_NAME).document(postId).delete();
+    public static Task<Void> deletePost(String postId) {
+        return PostHelper.getPostCollection().document(postId).delete();
     }
 }
